9b63db8ea0e8d6ab7db3031c3e1fec5d02bca82f
sarthak-21 <luthrasarthak83@gmail.com>
2022-12-07 02:20:36 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+import './App.css';
+
+function App() {
+
+  const [image, setImage] = useState('');
+  const [cropData, setCropData] = useState("#");
+  const [cropper, setCropper] = useState<any>();
+
+  const onChange = (e: any) => {
+    e.preventDefault();
+    let files;
+    
+    if(e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if(e.target) {
+      files = e.target.files;
+    }
+    
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result as any);
+    };
+    
+    reader.readAsDataURL(files[0]);
+  }
+
+  const getCropData = () => {
+    if(typeof cropper !== "undefined"){
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+    }
+  }
+
+  const defaultImage = () => {
+    setImage("./default.jpg")
+  } 
+
+  return (
+    <div className="App">
+      <div style={{ padding: "0 5%" }}>
+        <h1 style={{ textAlign: "center"}}>Image Cropper Tool</h1>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <input className="input-button" type="file" onChange={onChange} />
+          <button className="default-button" type="submit" onClick={defaultImage}>Default image</button>
+        </div>
+        <Cropper
+                style = {{ margin: "0 auto", maxHeight: "500px", maxWidth: "500px"}}
+                initialAspectRatio = {1}
+                preview = ".img-preview"
+                src = {image}
+                viewMode = {1}
+                minCropBoxHeight = {10}
+                minCanvasWidth = {10}
+                background = {false}
+                responsive = {true}
+                autoCropArea = {1}
+                checkOrientation = {false}
+                onInitialized = {(instance) => {
+                  setCropper(instance);
+                }}
+                guides = {true}
+        />
+        <div className="view-div">
+          <div className="box">
+            <h3>Preview</h3>
+            <div style={{ width: "300px" }} className="img-preview" />
+            <button className="button-style" type="submit" onClick={getCropData}>Crop Image</button>
+          </div>
+
+          <div className="box" style={{ width: "50%" }}>
+            <h3>Cropped Image</h3>
+            <img style={{ width: "300px" }} src={cropData} alt="cropped" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
